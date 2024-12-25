a23ac33a652b65e756017a4f8d56350a973ddd0e
Alice Jacka <jacka.alice@gmail.com>
2024-12-25 19:53:19 +1100

    Vickie: update hardware config

--- hosts/vickie/hardware-configuration.nix
+++ hosts/vickie/hardware-configuration.nix
@@ -4,29 +4,42 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-  ];
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/e158b40f-f448-4a29-804b-06c885ecad6c";
+      fsType = "btrfs";
+      options = [ "subvol=NixOS" ];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = [ "fmask=0077" "dmask=0077" ];
-  };
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/e158b40f-f448-4a29-804b-06c885ecad6c";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
 
-  swapDevices = [
-    { device = "/dev/disk/by-label/swap"; }
-  ];
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/e158b40f-f448-4a29-804b-06c885ecad6c";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/9349-D4FB";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/9d17e601-b394-4a91-988f-659c00f59dba"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
