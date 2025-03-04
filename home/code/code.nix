737e456b6760486e8471db51682b530fcd239eca
Alice Jacka <jacka.alice@gmail.com>
2025-03-04 14:00:22 +1100

    update vscode settings to new format

--- home/code/code.nix
+++ home/code/code.nix
@@ -7,41 +7,43 @@
       enable = true;
       package = pkgs.vscodium;
 
-      userSettings = import ./settings.nix;
-      keybindings = builtins.fromJSON (builtins.readFile ./keybindings.jsonc);
+      profiles.default = {
+        userSettings = import ./settings.nix;
+        keybindings = builtins.fromJSON (builtins.readFile ./keybindings.jsonc);
 
-      extensions = with pkgs.vscode-extensions; [
-        # Themes
-        pkief.material-icon-theme
-        github.github-vscode-theme
-        pkief.material-product-icons
-        oderwat.indent-rainbow
-        # kshetline.ligatures-limited
+        extensions = with pkgs.vscode-extensions; [
+          # Themes
+          pkief.material-icon-theme
+          github.github-vscode-theme
+          pkief.material-product-icons
+          oderwat.indent-rainbow
+          # kshetline.ligatures-limited
 
-        # Languages
-        yzhang.markdown-all-in-one
-        bierner.markdown-preview-github-styles
-        davidanson.vscode-markdownlint
+          # Languages
+          yzhang.markdown-all-in-one
+          bierner.markdown-preview-github-styles
+          davidanson.vscode-markdownlint
 
-        # oven.bun-vscode # TODO: not in nixpkgs
-        mechatroner.rainbow-csv
-        jnoortheen.nix-ide
-        ms-vscode.cpptools
-        streetsidesoftware.code-spell-checker
+          # oven.bun-vscode # TODO: not in nixpkgs
+          mechatroner.rainbow-csv
+          jnoortheen.nix-ide
+          ms-vscode.cpptools
+          streetsidesoftware.code-spell-checker
 
-        # Formatters
-        # Astyle
-        dbaeumer.vscode-eslint
-        visualstudioexptteam.vscodeintellicode
-        christian-kohler.path-intellisense
-        esbenp.prettier-vscode
+          # Formatters
+          # Astyle
+          dbaeumer.vscode-eslint
+          visualstudioexptteam.vscodeintellicode
+          christian-kohler.path-intellisense
+          esbenp.prettier-vscode
 
-        # Utility
-        aaron-bond.better-comments
-        usernamehw.errorlens
-        ms-vscode.hexeditor
-        mhutchie.git-graph
-      ];
+          # Utility
+          aaron-bond.better-comments
+          usernamehw.errorlens
+          ms-vscode.hexeditor
+          mhutchie.git-graph
+        ];
+      };
     };
   };
 }
