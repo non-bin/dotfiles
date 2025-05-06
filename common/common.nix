f057c76d4642e2ddfeec9bd061363a9f0b09471b
Alice Jacka <jacka.alice@gmail.com>
2025-05-06 10:19:48 +1000

    Steam

--- common/common.nix
+++ common/common.nix
@@ -167,6 +167,13 @@
       enable = true;
       binfmt = true; # Run appimages directly
     };
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    };
   };
 
   services = {
