3211ebfeaa41e9bfc0ba8300049fce7436cd68ad
Alice Jacka <jacka.alice@gmail.com>
2025-09-22 16:09:35 +1000

    change btrbk preserve settings

--- config/personal/modules/os/btrbk.nix
+++ config/personal/modules/os/btrbk.nix
@@ -13,7 +13,7 @@
           transaction_log = "/snapshots/btrbk.log"; # localtime type status target_url source_url parent_url message
           timestamp_format = "long";
           snapshot_preserve_min = "2d"; # Keep everything for at least 2d
-          snapshot_preserve = "48h 20d 10w 12m"; # Keep daily backups for 20 days, weeklys for 10 weeks, and monthlies for 12 months
+          snapshot_preserve = "168h 30d 26w 12m"; # Keep hourly backups for 1 week, dailies for a month, weeklies for 6 months, and monthlies for 12 months
           snapshot_create = "onchange"; # Don't create snapshots if nothing's changed
           volume."/" = {
             subvolume = "/home";
