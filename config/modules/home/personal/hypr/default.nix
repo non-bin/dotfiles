77bd3168275052bb29a2440531d237701d35c7d0
Alice Jacka <jacka.alice@gmail.com>
2025-12-03 21:55:43 +1100

    fix binary caches

--- config/modules/home/personal/hypr/default.nix
+++ config/modules/home/personal/hypr/default.nix
@@ -15,12 +15,6 @@
     ./wlogout.nix
   ];
 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   services.hyprpaper = {
     enable = true;
     settings = {
