2a436494ee76e059371b969934bdd63a1bb2e7c9
Alice Jacka <jacka.alice@gmail.com>
2025-12-19 13:28:06 +1100

    reload: add generation switcher

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -92,6 +92,7 @@ while [[ $# -gt 0 ]]; do
       echo "  -p, --pull        Pull updates from git before updating"
       echo "  -P, --push        Commit and push all changes"
       echo "  -r, --rebuild     Explicitly run the rebuild command (eg if running with -c or -o)"
+      echo "  -g, --generation  Interactively switch to a previous generation"
       echo "  -k, --rekey       Generate and rekey agenix secrets"
       echo "  -v, --vm          Build and run a VM with 'nixos-rebuild build-vm', and expose port 22 through the host's port 2221"
       echo "  -d, --dry-run     Run everything but the rebuild command"
@@ -159,7 +160,25 @@ if [ "$REKEY" == "YES" ]; then
 fi
 
 if [ "$GENERATION" == "YES" ]; then
-  bash $(home-manager generations | fzf | awk -F '-> ' '{print $2 "/activate"}')
+  if [ "$NIX_HOMEMAN_STANDALONE_TYPE" == "" ]; then
+    generation=$(nixos-rebuild list-generations | awk 'NR==1 { header=$0; next } 1' | fzf --header "$(nixos-rebuild list-generations | head -n1)" | awk '{print $1}')
+
+    if [ -z "$generation" ]; then
+      echo "No generation selected"
+      exit 1
+    fi
+
+    # if current generation is selected, do nothing
+    if [ "$(readlink /nix/var/nix/profiles/system)" = "system-$generation-link" ]; then
+      echo "Selected generation is already active"
+      exit 0
+    fi
+
+    sudo nix-env --switch-generation "$generation" -p /nix/var/nix/profiles/system
+    sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+  else
+    bash $(home-manager generations | fzf | awk -F '-> ' '{print $2 "/activate"}')
+  fi
 fi
 
 if [ "$DRY" != "YES" ] && [ "$REBUILD" == "YES" ]; then
