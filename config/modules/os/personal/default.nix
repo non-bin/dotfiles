e25f36f8f015b9e19bead31fa7e088ca13a6224d
Alice Jacka <jacka.alice@gmail.com>
2026-01-06 10:22:27 +1100

    update

--- config/modules/os/personal/default.nix
+++ config/modules/os/personal/default.nix
@@ -59,7 +59,7 @@
 
     thunar = {
       enable = true;
-      plugins = with pkgs.xfce; [
+      plugins = with pkgs; [
         thunar-archive-plugin
         thunar-volman
         thunar-media-tags-plugin
