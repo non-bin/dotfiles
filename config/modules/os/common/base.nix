a9c1c5cdc902ad417fa1fa5ec4cb41ca8d4fc38c
Alice Jacka <jacka.alice@gmail.com>
2026-04-15 13:40:02 +1000

    Add cuda binary cache

--- config/modules/os/common/base.nix
+++ config/modules/os/common/base.nix
@@ -27,14 +27,12 @@
     substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
-    ];
-    trusted-substituters = [
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 
