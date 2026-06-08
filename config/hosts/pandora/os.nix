15377afb18cd60ecc95255bd78ecebbd14934e70
Alice Jacka <jacka.alice@gmail.com>
2026-06-08 18:07:27 +1000

    Pandora: Bodge fix userPass

--- config/hosts/pandora/os.nix
+++ config/hosts/pandora/os.nix
@@ -1,5 +1,6 @@
 {
   user,
+  lib,
   ...
 }:
 
@@ -13,6 +14,7 @@
 
   # Obtain this using `ssh-keyscan` or by looking it up in your ~/.ssh/known_hosts
   age.rekey.hostPubkey = user.sshKeys.pandora;
+  age.secrets.userPass.rekeyFile = lib.mkForce null;
 
   networking.hostName = "pandora";
   networking.domain = "jacka.net.au";
