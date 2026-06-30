60ff0f3f3e328e077b2d14a9baa723ea20a4491b
Alice Jacka <jacka.alice@gmail.com>
2026-06-30 15:13:49 +1000

    dircolors: make OTHER_WRITABLE less obnoxious

--- config/modules/home/common/shell/default.nix
+++ config/modules/home/common/shell/default.nix
@@ -8,9 +8,37 @@
     ./zellij.nix
   ];
 
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-    options = [ "--cmd cd" ];
+  programs = {
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [ "--cmd cd" ];
+    };
+
+    dircolors = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      settings = {
+        RESET = "0";
+        DIR = "01;34";
+        LINK = "01;36";
+        MULTIHARDLINK = "00";
+        FIFO = "33";
+        SOCK = "01;35";
+        DOOR = "01;35";
+        BLK = "01;33";
+        CHR = "01;33";
+        ORPHAN = "01;31";
+        MISSING = "00";
+        SETUID = "37;41";
+        SETGID = "30;43";
+        CAPABILITY = "30;41";
+        STICKY_OTHER_WRITABLE = "30;42";
+        OTHER_WRITABLE = "33";
+        STICKY = "37;44";
+        EXEC = "01;32";
+      };
+    };
   };
 }
