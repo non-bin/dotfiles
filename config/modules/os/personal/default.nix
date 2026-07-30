6cfe610900a19efeee5425177861cdb63ddb3ae9
Alice Jacka <jacka.alice@gmail.com>
2026-07-30 11:04:03 +1000

    personal: enable print services

--- config/modules/os/personal/default.nix
+++ config/modules/os/personal/default.nix
@@ -40,6 +40,20 @@
       package = pkgs.transmission_4;
       enable = true;
     };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-filters
+        cups-browsed
+      ];
+    };
   };
 
   virtualisation = {
