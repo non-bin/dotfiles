4cf41730c818d89f024060b6d390304908bca48a
Alice Jacka <jacka.alice@gmail.com>
2026-09-25 19:18:53 +1000

    NOD

--- flake.nix
+++ flake.nix
@@ -10,12 +10,10 @@
     };
 
     nix-on-droid = {
-      url = "github:non-bin/nix-on-droid/testing";
-      # url = "github:non-bin/nix-on-droid/unstable";
-      # url = "github:nix-community/nix-on-droid/testing";
-      # url = "github:nix-community/nix-on-droid/prerelease-25.11";
-      # url = "github:nix-community/nix-on-droid/master";
+      # url = "github:nix-community/nix-on-droid";
+      url = "github:non-bin/nix-on-droid";
 
+      inputs.home-manager.follows = "home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
